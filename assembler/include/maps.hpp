8e6736f37d1382e83aabf9ea80058fea33e0e33d
Igor Pontes Tresolavy <tresolavy@usp.br>
2024-07-14 02:14:59 -0300

    wip: inserts maps.hpp and types.hpp

---
+++ assembler/include/maps.hpp
@@ -0,0 +1,11 @@
+#ifndef MAPS_HPP
+#define MAPS_HPP
+
+#include "types.hpp"
+#include <string>
+#include <unordered_map>
+
+std::unordered_map<std::string, poli_as::DataProcessing>
+    data_processing_instruction_map;
+
+#endif // MAPS_HPP

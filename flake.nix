13ba3f210a32174d8510a4430298c5e28fbf33bb
Thiago souza <thifeandfes0304@gmail.com>
2024-08-08 16:04:30 -0300

    added nix support

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "A flake to set up a development environment with Rust and other dependencies";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+      ...
+    }:
+    let
+      rust_overlay = import (
+        builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz"
+      );
+      pkgs = import <nixpkgs> { overlays = [ rust_overlay ]; };
+      rustVersion = "latest";
+      #rustVersion = "1.62.0";
+      rust = pkgs.rust-bin.stable.${rustVersion}.default.override {
+        extensions = [
+          "rust-src" # for rust-analyzer
+          "rust-analyzer"
+        ];
+      };
+    in
+    {
+      devShells.aarch64-darwin.default = pkgs.mkShell {
+        buildInputs =
+          [ rust ]
+          ++ (with pkgs; [
+            pkg-config
+            # other dependencies
+            #gtk3
+            #wrapGAppsHook
+          ]);
+        RUST_BACKTRACE = 1;
+      };
+    };
+}

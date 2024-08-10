d023d6a65c331ea5fc1af6191ab9c00a67880955
Thiago souza <thifeandfes0304@gmail.com>
2024-08-10 10:36:51 -0300

    fix: cleanup

--- src/elf/elf_writer.rs
+++ src/elf/elf_writer.rs
@@ -1,34 +1,29 @@
-use object::elf::{EM_ARM, ET_REL};
 use object::write::elf::Writer;
-use object::write::elf::{FileHeader, SectionHeader};
-use object::write::{Object, StreamingBuffer, Symbol, SymbolSection};
+use object::write::{Object, StreamingBuffer};
 use object::{Architecture, Endianness};
-use std::collections::HashMap;
 use std::fs::File;
-use std::io::{self, BufWriter, Write};
+use std::io::BufWriter;
 
 pub struct ElfWriter<'a> {
     object: Object<'a>,
-    file_name: String,
     streaming_buffer: StreamingBuffer<BufWriter<File>>,
 }
 
 impl<'a> ElfWriter<'a> {
-    fn new(file_name: String) -> ElfWriter<'a> {
+    pub fn new(file_name: String) -> ElfWriter<'a> {
         let endianess = Endianness::Little;
         let object = Object::new(object::BinaryFormat::Elf, Architecture::Arm, endianess);
-        let file = File::create(file_name.clone()).expect("Was not able to create output file");
+        let file = File::create(file_name).expect("Was not able to create output file");
         let buf_writer = BufWriter::new(file);
         let streaming_buffer = StreamingBuffer::new(buf_writer);
 
         ElfWriter {
             object,
-            file_name,
             streaming_buffer,
         }
     }
 
-    fn write(&mut self) -> Writer {
+    pub fn write(&mut self) -> Writer {
         Writer::new(Endianness::Little, false, &mut self.streaming_buffer)
     }
 }
